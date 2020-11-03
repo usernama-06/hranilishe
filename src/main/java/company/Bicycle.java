39d53e1c22114faa2c6ac04c7261ab1930285b54
aleksey_krapivnitskiy <krapivnitskiy@secureducation.ru>
2020-11-03 10:34:17 +0300

    added todo's

--- src/main/java/company/Bicycle.java
+++ src/main/java/company/Bicycle.java
@@ -5,6 +5,8 @@ public abstract class Bicycle {
     //todo change to interface
     //todo create abstract class
     //todo code style
+    //todo remove .idea, .iml from repository
+    //todo fix namings in whole project
     public void RIDE (){
 
     };
